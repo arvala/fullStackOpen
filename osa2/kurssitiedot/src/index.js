5315320599b7b22c8499fae95c8face2412b4f61
arvala <arvala@utu.fi>
2019-08-06 17:54:28 +0300

    task 2.4: Added Courses-component which renders courses separately. Now courses can be added at will.

--- osa2/kurssitiedot/src/index.js
+++ osa2/kurssitiedot/src/index.js
@@ -2,48 +2,91 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-    const course = {
-      name: 'Half Stack application development',
-      parts: [
+    const courses = [
         {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
+            name: 'Half Stack application development',
+            id: 1,
+            parts: [
+                {
+                    name: 'Fundamentals of React',
+                    exercises: 10,
+                    id: 1
+                },
+                {
+                    name: 'Using props to pass data',
+                    exercises: 7,
+                    id: 2
+                },
+                {
+                    name: 'State of a component',
+                    exercises: 14,
+                    id: 3
+                },
+                {
+                    name: 'Redux',
+                    exercises: 11,
+                    id: 4
+                },
+                {
+                    name: 'Additional part',
+                    exercises: 11,
+                    id: 5
+                }
+            ]
         },
         {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
+            name: 'Node.js',
+            id: 2,
+            parts: [
+                {
+                    name: 'Routing',
+                    exercises: 3,
+                    id: 1
+                },
+                {
+                    name: 'Middlewares',
+                    exercises: 7,
+                    id: 2
+                }
+            ]
         },
         {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        },
-        {
-            name: 'Additional part',
-            exercises: 11,
-            id: 5
-          }
-      ]
-    }
-  
-    return (
-      <div>
-        <Course course={course} />
-      </div>
-    )
-  }
+            name: 'Bullshit.js',
+            id: 3,
+            parts: [
+                {
+                    name: 'Extra1',
+                    exercises: 4,
+                    id: 1
+                },
+                {
+                    name: 'Extra2',
+                    exercises: 8,
+                    id: 2
+                }
+            ]
+        }
+    ]
 
-const Course = ({course}) => {
     return (
         <div>
-            <Header course={course}/>
+            <Courses courses={courses} />
+        </div>
+    )
+}
+
+const Courses = ({courses}) => {
+    return (
+        <>
+         {courses.map(course => <Course key={course.id} course={course} />)}
+        </>
+    )
+}
+
+const Course = ({ course }) => {
+    return (
+        <div>
+            <Header course={course} />
             <Content parts={course.parts} />
             <Total parts={course.parts} />
         </div>
@@ -51,36 +94,36 @@ const Course = ({course}) => {
 }
 
 
-const Header = ({course}) =>{
+const Header = ({ course }) => {
     return (
         <>
-        <h1>{course.name}</h1>
-        </>
-    )
-} 
-
-const Content = ({parts}) =>{
-    return (
-        <>
-        {parts.map(part => <Part key={part.id} part = {part.name} exercises = {part.exercises}/>)}
-        </>
-    )
-} 
-
-const Part = ({part, exercises}) =>{
-    return(
-        <>
-        <p>{part} {exercises}</p>
+            <h1>{course.name}</h1>
         </>
     )
 }
 
-const Total = ({parts}) =>{
+const Content = ({ parts }) => {
+    return (
+        <>
+            {parts.map(part => <Part key={part.id} part={part.name} exercises={part.exercises} />)}
+        </>
+    )
+}
+
+const Part = ({ part, exercises }) => {
+    return (
+        <>
+            <p>{part} {exercises}</p>
+        </>
+    )
+}
+
+const Total = ({ parts }) => {
     console.log(parts)
     var totalAmount = parts.reduce((sum, part) => sum + part.exercises, 0)
-    return(
+    return (
         <>
-        <p>Yhteensä {totalAmount} tehtävää</p>
+            <p>Yhteensä {totalAmount} tehtävää</p>
         </>
     )
 }
